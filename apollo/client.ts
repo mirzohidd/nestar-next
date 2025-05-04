33adc5f4a050d2e68ca4c21c8bdbbed561b8e2f5
mirzohidd <mirzohidmurodov77@gmail.com>
2025-05-04 17:29:14 +1000

    feat: develop WebSocket client chatting first stage

--- apollo/client.ts
+++ apollo/client.ts
@@ -27,7 +27,30 @@ const tokenRefreshLink = new TokenRefreshLink({
 		return null;
 	},
 });
+// Custom  WebSocket client
+class LoggingWebSocket {
+	private socket: WebSocket;
 
+	constructor(url: string) {
+		this.socket = new WebSocket(url);
+
+		this.socket.onopen = () => {
+			console.log('WebSocket connection !');
+		};
+		this.socket.onmessage = (msg) => {
+			console.log('WebSocket message:', msg.data);
+		};
+		this.socket.onerror = (error) => {
+			console.log('WebSocket error:', error);
+		};
+	}
+	send(data: string | ArrayBuffer | SharedArrayBuffer | Blob | ArrayBuffer) {
+		this.socket.send(data);
+	}
+	close() {
+		this.socket.close();
+	}
+}
 function createIsomorphicLink() {
 	if (typeof window !== 'undefined') {
 		const authLink = new ApolloLink((operation, forward) => {
@@ -56,6 +79,7 @@ function createIsomorphicLink() {
 					return { headers: getHeaders() };
 				},
 			},
+			webSocketImpl: LoggingWebSocket,
 		});
 
 		const errorLink = onError(({ graphQLErrors, networkError, response }) => {
